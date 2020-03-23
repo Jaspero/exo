d25fe7128b80eb719ff07d3771962b2215770124
bobo <adelcanin@hotmail.com>
2020-03-23 14:52:07 +0100

    added stlye for inputs and contact page

--- app/src/app/contact-us/contact-us.component.ts
+++ app/src/app/contact-us/contact-us.component.ts
@@ -8,6 +8,49 @@ import { Component, OnInit } from '@angular/core';
 export class ContactUsComponent implements OnInit {
 
   constructor() { }
+exo = {
+  services: [
+    {
+      name: 'EXO Lounge',
+    },
+    {
+      name: 'EXO Nail Bar',
+    }
+  ]
+};
+
+week = {
+  days: [
+    {
+     day: 'Monday',
+     time: '8:30am to 5pm',
+    },
+    {
+      day: 'Tuesday',
+      time: '8:30am to 5pm',
+    },
+    {
+      day: 'Wednesday',
+      time: '8:30am to 5pm',
+    },
+    {
+      day: 'Thursday',
+      time: '8:30am to 5pm',
+    },
+    {
+      day: 'Friday',
+      time: '8:30am to 5pm',
+    },
+    {
+      day: 'Saturday',
+      time: 'Closed',
+    },
+    {
+      day: 'Sunday',
+      time: 'Closed',
+    },
+  ]
+};
 
   ngOnInit(): void {
   }
--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -9,7 +9,23 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  links = {
+    backgrounds:  [
+      {
+        name: 'Events',
+        background: 'assets/images/home/home-3.png'
+      },
+      {
+        name: 'Blog',
+        background: 'assets/images/home/home-4.png'
+      },
+      {
+        name: 'News & Press',
+        background: 'assets/images/home/home-5.png'
+      }
+    ]
   }
 
+  ngOnInit(): void {
+  }
 }
