ff0199c93cbaf472081b34cf9338e9ac2d8610c2
bobo <adelcanin@hotmail.com>
2020-03-30 15:08:13 +0200

    chore(blog) hotfix

--- app/src/app/blog/blog.module.ts
+++ app/src/app/blog/blog.module.ts
@@ -4,9 +4,10 @@ import {ScullyLibModule} from '@scullyio/ng-lib';
 import {BlogRoutingModule} from './blog-routing.module';
 import {BlogComponent} from './components/blog/blog.component';
 import {BlogListComponent} from './components/blog-list/blog-list.component';
+import {SharedModule} from "../shared.module";
 
 @NgModule({
   declarations: [BlogComponent, BlogListComponent],
-  imports: [CommonModule, BlogRoutingModule, ScullyLibModule]
+  imports: [CommonModule, BlogRoutingModule, ScullyLibModule, SharedModule]
 })
 export class BlogModule {}
