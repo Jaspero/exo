64741d06e1f174fa4c096da3eba04fc0af65c131
bobo <adelcanin@hotmail.com>
2020-04-07 14:26:05 +0200

    chore(news-list)adding news dynamically

--- app/src/app/news/component/news-list/news-list.component.ts
+++ app/src/app/news/component/news-list/news-list.component.ts
@@ -12,6 +12,31 @@ import {map} from 'rxjs/operators';
 export class NewsListComponent implements OnInit {
   constructor(public scully: ScullyRoutesService) {}
 
+  news = {
+    news: [
+      {
+        newsImg: 'assets/images/news/news-exo.png',
+        title: 'Exo Menu Lounge',
+        date: 'Date: 16.03. 2020',
+        category: 'Nail-bar',
+        newsDescription: 'EXO Industries is a brand of lifestyle services. The company currently consists of EXO Loun' +
+          'ge, a unique cocktail lounge that serves organic, healthy cocktails and EXO Nail Bar, a full-service nail b' +
+          'ar dedicated to consistently providing high customer satisfaction by rendering excellent service, quality o' +
+          'rganic products and furnishing an enjoyable atmosphere that appeals to the senses.'
+      },
+      {
+        newsImg: 'assets/images/news/news-lounge.png',
+        title: 'Exo Menu Lounge',
+        date: 'Date: 16.03. 2020',
+        category: 'Lounge',
+        newsDescription: 'EXO Industries is a brand of lifestyle services. The company currently consists of EXO Loun' +
+          'ge, a unique cocktail lounge that serves organic, healthy cocktails and EXO Nail Bar, a full-service nail b' +
+          'ar dedicated to consistently providing high customer satisfaction by rendering excellent service, quality o' +
+          'rganic products and furnishing an enjoyable atmosphere that appeals to the senses.'
+      },
+    ]
+  };
+
   news$: Observable<ScullyRoute[]>;
 
   ngOnInit() {
