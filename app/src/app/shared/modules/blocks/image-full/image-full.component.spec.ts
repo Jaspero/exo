a6366b138db51553fd3b41d6429c4044842e2a25
bobo <adelcanin@hotmail.com>
2020-04-13 13:04:25 +0200

    chore()hotfix

---
+++ app/src/app/shared/modules/blocks/image-full/image-full.component.spec.ts
@@ -0,0 +1,24 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {ImageFullComponent} from './image-full.component';
+
+describe('ImageFullComponent', () => {
+  let component: ImageFullComponent;
+  let fixture: ComponentFixture<ImageFullComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ImageFullComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ImageFullComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ app/src/app/shared/modules/blocks/image-full/image-full.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'exo-image-full',
+  templateUrl: './image-full.component.html',
+  styleUrls: ['./image-full.component.scss']
+})
+export class ImageFullComponent {
+  data: any;
+}
