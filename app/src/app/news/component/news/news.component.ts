021732927234af61d8515bc4d6b0a0a3aecdf549
bobo <adelcanin@hotmail.com>
2020-04-20 09:59:51 +0200

    chore()added intro segment for all the pages

--- app/src/app/news/component/news/news.component.ts
+++ app/src/app/news/component/news/news.component.ts
@@ -1,4 +1,9 @@
-import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
 declare var ng: any;
@@ -15,6 +20,12 @@ export class NewsComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
   item: any;
+  viewMode = 'tab1';
+  activeImage: string;
+
+  get bg() {
+    return {'background-image': `url(${this.item.displayImage})`};
+  }
 
   ngOnInit() {
     this.item = this.route.snapshot.data.meta;
