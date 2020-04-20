a06d7592f9a5ccc2d161b3b8245c9aac98b76adc
bobo <adelcanin@hotmail.com>
2020-04-20 15:12:15 +0200

    chore()fixing different style issues,added animation on cotnact-us page

--- app/src/app/contact-us/contact-us.module.ts
+++ app/src/app/contact-us/contact-us.module.ts
@@ -3,13 +3,15 @@ import {SharedModule} from '../shared.module';
 import {MarkdownModule} from '../shared/modules/markdown/markdown.module';
 import {ContactUsRoutingModule} from './contact-us-routing.module';
 import {ContactUsComponent} from './contact-us.component';
+import {IntersectionModule} from '../shared/modules/intersecting/intersection.module';
 
 @NgModule({
   declarations: [ContactUsComponent],
   imports: [
     SharedModule,
     ContactUsRoutingModule,
-    MarkdownModule
+    MarkdownModule,
+    IntersectionModule
   ]
 })
 export class ContactUsModule {}
