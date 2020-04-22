2dd223ff471aebef61a4ad6e3fbaa813bc7aa91a
Filip Lauc <filip.lauc93@gmail.com>
2020-04-22 18:01:21 +0200

    fix: lint

--- app/src/app/shared/modules/blocks/events-block/events-block.component.ts
+++ app/src/app/shared/modules/blocks/events-block/events-block.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'exo-events-block',
@@ -6,7 +6,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
   styleUrls: ['./events-block.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EventsBlockComponent {
+export class EventsBlockComponent implements OnInit {
   data: any;
 
   blocks: any[];
