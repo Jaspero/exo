1979d86f996cea58263e9b004191730dd69f6ff7
bobo <adelcanin@hotmail.com>
2020-05-15 12:26:23 +0200

    chore(gallery-block)layout to gallery-block

--- app/src/app/shared/modules/blocks/gallery-block/gallery-block.component.ts
+++ app/src/app/shared/modules/blocks/gallery-block/gallery-block.component.ts
@@ -1,12 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'exo-gallery-block',
   templateUrl: './gallery-block.component.html',
   styleUrls: ['./gallery-block.component.scss']
 })
-export class GalleryBlockComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class GalleryBlockComponent {
+  data: any;
 }
